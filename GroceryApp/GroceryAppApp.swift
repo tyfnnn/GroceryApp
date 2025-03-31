983fd79625d7e55ea0f2bbcb1098a41b698edf7d
tyfnnn <tayfun.i@icloud.com>
2025-03-31 10:45:26 +0200

    Initial commit

---
+++ GroceryApp/GroceryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryAppApp.swift
+//  GroceryApp
+//
+//  Created by Tayfun Ilker on 31.03.25.
+//
+
+import SwiftUI
+
+@main
+struct GroceryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GroceryAppTests/GroceryAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryAppTests.swift
+//  GroceryAppTests
+//
+//  Created by Tayfun Ilker on 31.03.25.
+//
+
+import Testing
+@testable import GroceryApp
+
+struct GroceryAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
