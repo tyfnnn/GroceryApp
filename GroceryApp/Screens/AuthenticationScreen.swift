04fe1eccc6332d5be479e27d04b2c43fe04c6a11
tyfnnn <tayfun.i@icloud.com>
2025-06-22 21:38:38 +0200

    Modern AuthenticationScreen

---
+++ GroceryApp/Screens/AuthenticationScreen.swift
@@ -0,0 +1,18 @@
+//
+//  AuthenticationScreen.swift
+//  GroceryApp
+//
+//  Created by Tayfun Ilker on 22.06.25.
+//
+
+import SwiftUI
+
+struct AuthenticationScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AuthenticationScreen()
+}
